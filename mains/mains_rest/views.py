5fbff06f4db9486b7c7699c89938f9fe4638449d
kapitan-alexey <Shmateyevne@gmail.com>
2022-07-11 12:33:56 +0600

    all done but endpoints

--- mains/mains_rest/views.py
+++ mains/mains_rest/views.py
@@ -4,9 +4,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .services import import_xls_clients
+from .services import process_client_org_xlsx, process_bills_xlsx
 
-from .models import Clients, create_client, create_organization
+from .models import add_client, add_organization, add_bill
 
 
 # Create your views here.
@@ -19,11 +19,24 @@ class ImportClientsApiView(APIView):
     def post(self, request):
         xls = request.FILES['filename']
 
-        clients_to_create, organizations_to_create = import_xls_clients(xls)
+        clients_to_add, organizations_to_add = process_client_org_xlsx(xls)
 
-        create_client(clients_to_create)
-        create_organization(organizations_to_create)
+        add_client(clients_to_add)
+        add_organization(organizations_to_add)
 
-        print(Clients.objects.all())
+        return Response({'status': 'file received'}, status=status.HTTP_201_CREATED)
+
+
+class ImportBillsApiView(APIView):
+
+    def get(self, request):
+        return Response({'status': 'ok'}, status.HTTP_200_OK)
+
+    def post(self, request):
+        xls = request.FILES['filename']
+
+        bills_to_add = process_bills_xlsx(xls)
+
+        add_bill(bills_to_add)
 
         return Response({'status': 'file received'}, status=status.HTTP_201_CREATED)
